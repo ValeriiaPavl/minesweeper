c2ae65bea2c9eab71906c2f9facb1834928f9280
Valeriia Pavlova <happyamanita@gmail.com>
2023-10-25 15:43:04 +0200

    added an animation to the app-logo

--- components/App.tsx
+++ components/App.tsx
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <div className="Minesweeper">
+        <div className="header">
+          <h1> Minesweeper</h1>
+          <img className="bomb" src="bomb.svg" />
+        </div>
+
         <Field />
       </div>
     </div>
