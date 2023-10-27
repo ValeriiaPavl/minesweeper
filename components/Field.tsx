832c03ca9c07b97dc25e82dda3fd1282d022b9da
Valeriia Pavlova <happyamanita@gmail.com>
2023-10-27 14:37:25 +0200

    removed key property from cell component

--- components/Field.tsx
+++ components/Field.tsx
@@ -2,31 +2,29 @@ import React, { useState } from "react";
 
 interface CellProps {
   value: number;
-  key: string;
 }
 
-const Cell = ({ value, key }: CellProps) => {
+const Cell = ({ value }: CellProps) => {
   const [cellCondition, setCondition] = useState<"closed" | "open" | "flagged">(
     "closed"
   );
 
   if (cellCondition === "open") {
     return (
-      <div key={key} className="cell cell-open">
+      <div className="cell cell-open">
         <p>{value}</p>
       </div>
     );
   } else {
     if (cellCondition === "flagged") {
       return (
-        <div key={key} className="cell cell-flagged">
+        <div className="cell cell-flagged">
           <p>{value}</p>
         </div>
       );
     }
     return (
       <div
-        key={key}
         onClick={() => setCondition("open")}
         onContextMenu={() => setCondition("flagged")}
         className="cell"
@@ -43,10 +41,15 @@ function Field() {
       .fill(start)
       .map((x, y) => x + y * step);
   const fieldArray = range(1, 72, 1);
-  const fieldItems = fieldArray.map((number) => (
-    <Cell key={"{number.toString()}_cell"} value={number} />
-  ));
-  return <div className="cell-container">{fieldItems}</div>;
+
+
+  return (
+    <div className="cell-container">
+      {fieldArray.map((number) => (
+        <Cell key={`${number.toString()}_cell`} value={number} />
+      ))}
+    </div>
+  );
 }
 
 export default Field;
